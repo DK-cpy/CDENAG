10f2278d5256769e3f93eeb236987ac33efe97e0
DK-cpy <160464958+DK-cpy@users.noreply.github.com>
2026-05-25 10:04:37 +0800

    Add files via upload

---
+++ NAS-Bench-101/config/config.py
@@ -0,0 +1,20 @@
+hyper_params_setting = {
+    "num_step": 20,
+    "population_num": 30,
+    "geno_shape": (7, 7),
+    "temperature": 1.0,
+    "noise_scale": 0.8,
+    "mutate_rate": 0.6,
+    "elite_rate": 0.1,
+    "diver_rate": 0.2,
+    "mutate_distri_index": 5,
+    "rand_exp_num": 20,
+    "max_iter_time": 30,
+    "save_dir": "./results/",
+    "seed": [
+            222,
+            3456,
+            6006,
+            78,
+    ],
+}
